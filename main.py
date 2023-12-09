e84a4184a4bc287acd6a5445fc4761b58e2443d0
gui1416 <gui1416@github.com>
2023-12-09 15:10:58 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+aleatorio=random.randint(1,10)
+palpite=int(input("Entre com seu palpite de 1 a 10:"))
+
+print("Número sorteado:",aleatorio)
+
+
+if palpite>aleatorio:
+    print("Puxa, você chutou alto D+")
+if palpite<aleatorio:
+    print("Puxa, você chutou baixo D+")
+if palpite==aleatorio:
+    print("Aeeee!!!, acertou em cheio!")
